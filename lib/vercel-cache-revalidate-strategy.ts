93d9d8876e560bd19d09f6f0fce2c534b558efb1
Silvano Stralla <silvano.stralla@sistrall.it>
2024-05-24 14:10:16 +0200

    Remove log

--- lib/vercel-cache-revalidate-strategy.ts
+++ lib/vercel-cache-revalidate-strategy.ts
@@ -3,7 +3,6 @@ import { CacheTag } from "./cache-tags";
 
 export function revalidateQueriesUsingCacheTags(cacheTags: CacheTag[]) {
   for (const tag of cacheTags) {
-    console.log(`About to revalidate ${tag}`);
     revalidateTag(tag);
   }
 }
