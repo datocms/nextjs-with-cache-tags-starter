28894b417723071a2516badfe1fe45f45c949adb
Silvano Stralla <silvano.stralla@sistrall.it>
2024-05-24 13:59:43 +0200

    DRY the code

--- lib/fetch-contents.ts
+++ lib/fetch-contents.ts
@@ -1,19 +1,12 @@
 import { createHash } from "crypto";
 import {
+  CacheTag,
   associateFetchIdToTags,
   parseCommaSeparatedTagString,
 } from "./cache-tags";
 
-/**
- * `executeQuery` uses `fetch` to make a request to the
- * DatoCMS GraphQL API
- */
-export async function executeQuery(query = "", variables = {}) {
-  if (!query) {
-    throw new Error(`Query is not valid`);
-  }
-
-  const response = await fetch("https://graphql.datocms.com/", {
+async function fetchFromDatoCMS(query: string, variables = {}, tags: CacheTag[]) {
+  return fetch("https://graphql.datocms.com/", {
     method: "POST",
     // Headers are used to instruct DatoCMS on how to treat the request:
     headers: {
@@ -27,7 +20,22 @@ export async function executeQuery(query = "", variables = {}) {
     body: JSON.stringify({ query, variables }),
     // Next uses some reasonable default for caching, but we explicite them all
     cache: "force-cache",
+    next: {
+      tags,
+    }
   });
+}
+
+/**
+ * `executeQuery` uses `fetch` to make a request to the
+ * DatoCMS GraphQL API
+ */
+export async function executeQuery(query = "", variables = {}) {
+  if (!query) {
+    throw new Error(`Query is not valid`);
+  }
+
+  const response = await fetchFromDatoCMS(query, variables, [])
 
   if (!response.ok) {
     throw new Error(`Failed to fetch data: ${JSON.stringify(response)}`);
@@ -49,24 +57,16 @@ export async function executeQuery(query = "", variables = {}) {
     response.headers.get("x-cache-tags"),
   );
 
-  await fetch("https://graphql.datocms.com/", {
-    method: "POST",
-    // Headers are used to instruct DatoCMS on how to treat the request:
-    headers: {
-      // - No token, no party: only authorized requests return data
-      Authorization: `Bearer ${process.env.PUBLIC_DATOCMS_API_TOKEN}`,
-      // - Only return valid record
-      "X-Exclude-Invalid": "true",
-      // - Finally, return the cache tags together with the content.
-      "X-Cache-Tags": "true",
-    },
-    body: JSON.stringify({ query, variables }),
-    // Next uses some reasonable default for caching, but we explicite them all
-    cache: "force-cache",
-    next: {
-      tags,
-    },
-  });
+  /**
+   * We strongly leverage request memoization here: what follows is the same
+   * identical request we did before: we only add the cache tags we just
+   * retrieved.
+   *
+   * What happens behind the curtains is that `fetch` leverages the request
+   * cache (so no second call to DatoCMS) and marks the request with the tags we
+   * pass: it's a win-win! 
+   */
+  await fetchFromDatoCMS(query, variables, tags)
 
   /**
    * For educational purpose, tags are returned together with the data: in a
